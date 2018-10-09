50bd37d69b84542a915707c040b05c4daaa651bf
WonderLander <m.wenzl.1011@gmail.com>
2018-10-09 12:12:53 +0200

    almost all the tasks done

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,6 +1,10 @@
 package cz.muni.fi.pa165.currency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Currency;
 
 
@@ -12,7 +16,7 @@ import java.util.Currency;
 public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     private final ExchangeRateTable exchangeRateTable;
-    //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     public CurrencyConvertorImpl(ExchangeRateTable exchangeRateTable) {
         this.exchangeRateTable = exchangeRateTable;
@@ -20,7 +24,21 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        logger.trace("[" + this.getClass().toString() + "/" + "convert" + "] method start");
+        if (sourceCurrency == null || targetCurrency == null || sourceAmount == null) {
+            throw new IllegalArgumentException("Something is null");
+        }
+        try {
+            BigDecimal exchangeRate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+            if (exchangeRate == null) {
+                logger.warn("[" + this.getClass().toString() + "/" + "convert" + "] Unknown exchange rate");
+                throw new UnknownExchangeRateException("ExchangeRate is unknown");
+            }
+            return exchangeRate.multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
+        } catch (ExternalServiceFailureException ex) {
+            logger.error("[" + this.getClass().toString() + "/" + "convert" + "] method start");
+            throw new UnknownExchangeRateException("Error when fetching exchange rate", ex);
+        }
     }
 
 }
